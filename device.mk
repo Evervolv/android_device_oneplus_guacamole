7cd6f3165d329bf4d56331b2c511d57068adeb8b
Nick Reuter <nreuter85@gmail.com>
2016-11-10 15:56:09 -0600

    guacamole: initial setup to support aosp
    Change-Id: I17da1754471e38c8bd8e2c278ba595d5acaa8f4d

--- device.mk
+++ device.mk
@@ -14,30 +14,23 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_mixer_paths.xml
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
-
 # Camera
 PRODUCT_PACKAGES += \
     OnePlusCameraHelper
+
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
 
 # Device init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
-
-# Shipping API
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
@@ -45,6 +38,3 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8150-common/common.mk)
-
-# Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
--- lineage_guacamole.mk
+++ lineage_guacamole.mk
@@ -4,12 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from guacamole device
-$(call inherit-product, device/oneplus/guacamole/device.mk)
+$(call inherit-product, device/oneplus/guacamole/aosp_guacamole.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
