62e45ab27efea21a32fa733608b4c41936504fd5
Nick Reuter <nreuter85@gmail.com>
2016-11-10 15:56:09 -0600

    guacamole: initial setup to support aosp
    Change-Id: I17da1754471e38c8bd8e2c278ba595d5acaa8f4d

--- device.mk
+++ device.mk
@@ -14,13 +14,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_mixer_paths.xml
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
-
 # Camera
 PRODUCT_PACKAGES += \
     OnePlusCameraHelper
+
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
 
 # Device init scripts
 PRODUCT_PACKAGES += \
@@ -33,17 +32,11 @@ PRODUCT_COPY_FILES += \
 TARGET_BOARD_FASTBOOT_INFO_FILE := $(LOCAL_PATH)/fastboot-info.txt
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
-
-# Shipping API
-PRODUCT_SHIPPING_API_LEVEL := 28
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
@@ -51,6 +44,3 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8150-common/common.mk)
-
-# Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
