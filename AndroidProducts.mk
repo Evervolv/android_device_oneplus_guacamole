61fb3596feec989ed3abeaff20c82df0a0291895
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    guacamole: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,3 +7,11 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_guacamole.mk \
     $(LOCAL_DIR)/lineage_guacamole.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_guacamole-eng \
+    aosp_guacamole-userdebug \
+    aosp_guacamole-user \
+    lineage_guacamole-eng \
+    lineage_guacamole-userdebug \
+    lineage_guacamole-user
