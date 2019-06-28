b147bb0a7202c1589a54c7cad9eebec3f2d7cacc
Nick Reuter <nreuter85@gmail.com>
2019-06-27 23:11:22 -0500

    guacamole: convert to Evervolv
    Change-Id: I2f91bc02bc4bb4ab1dc3e96094631464f9b969bd

--- ev_guacamole.mk
+++ ev_guacamole.mk
@@ -11,15 +11,18 @@ $(call inherit-product, device/oneplus/guacamole/aosp_guacamole.mk)
 TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
+# Codename
+PRODUCT_CODENAME := Expletus
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-ev
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common evervolv stuff.
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamole
+PRODUCT_NAME := ev_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := GM1911
